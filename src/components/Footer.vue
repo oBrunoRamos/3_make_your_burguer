26fe942a3a563d03067a05052c1367806e4bfefd
Bruno Ramos <125314540+oBrunoRamos@users.noreply.github.com>
2023-09-04 22:21:10 -0300

    Aula22

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,27 @@
 <template>
-    <p>Footer</p>
+    <footer id="footer">
+        <p>Make You Burguer &copy; 2023</p>
+    </footer >
 </template>
 <script>
 export default {
     name: 'Footer',
-    data() {
-        return{
-
-        }
-    },
 }
 </script>
+<style scoped>
+    #footer{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        bottom: 0;
+
+        width: 100%;
+        height: 200px;
+
+        color: #fcba03;
+
+        background: #222;
+        border-top: 4px solid #111;
+    }
+</style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,19 +1,46 @@
 <template>
-    <nav>
-        <router-link to="/">
-            <img src="" id="logo">
+    <nav id="nav">
+        <router-link to="/" id="logo_url">
+            <img :src="logo" :alt="alt" id="logo">
         </router-link>
-        <router-link to="/">Home</router-link> |
+        <router-link to="/">Home</router-link>
         <router-link to="/pedidos">Pedidos</router-link>
-    </nav>
+    </nav >
 </template>
 <script>
-export default {
-    name: 'Navbar',
-    setup() {
-        
-    },
-}
+    export default {
+        name: 'Navbar',
+        props: ['logo', 'alt']
+    }
 </script>
-<style>
+<style scoped>
+    #nav{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        padding: 15px 50px;
+        
+        background-color: #222;
+        border-bottom: 4px solid #111;
+    }
+    #nav #logo_url{
+        margin: auto;
+        margin-left: 0;
+    }
+    #logo{
+        width: 40px;
+        height: 40px;
+    }
+    #nav a{
+        margin: 12px;
+
+        color: #fcba03;
+        text-decoration: none;
+
+        transition: .5s;
+    }
+    #nav a:hover{
+        color: #fff;
+    }
 </style>
